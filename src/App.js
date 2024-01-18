01430366c8fa5e1d4bf675019fee40df58494974
Ram Wagle <rkwagle@gmail.com>
2024-01-18 16:10:26 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Hello TaxBase!!
+          Hello TaxBase V2!!
       </header>
     </div>
   );
